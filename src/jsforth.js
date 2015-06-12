dc2e9028a4525efbb1a8e27c53de09aabe32af95
Rino Jose <rjose@spreadsheetkit.com>
2015-06-11 21:16:25 -0700

    Finish pass3

--- src/jsforth.js
+++ src/jsforth.js
@@ -5,7 +5,8 @@ $f = (function makeInterpreter() {
     var m_dictionary = {};              // Contains forth words
     var m_stack = [];                   // Main stack
     var m_return_stack = [];            // Return stack (for colon definition execution)
-    var m_variables = {};               // Stores variables (and constants)
+    var m_variables = {};               // Stores variables
+    var m_constants = {};               // Stores constants
 
     var m_input = "";                   // Current input string
     var m_input_index = 0;              // Index into m_input
@@ -55,21 +56,95 @@ $f = (function makeInterpreter() {
 	}
     }
 
+    //---------------------------------------------------------------------------
+    // Resets forth stack and return stack
+    //
+    // Also prints message to console
+    //---------------------------------------------------------------------------
+    function abort(message) {
+	m_stack = [];
+	m_return_stack = [];
+	console.log(message);
+    }
+
+
+    //---------------------------------------------------------------------------
+    // Reads next word and attempts to interpret it
+    //
+    // If the word isn't found, try to convert to a number. If not a number,
+    // then call abort.
+    //
+    // Returns 1 if a word was intepreted; 0 otherwise
+    //---------------------------------------------------------------------------
+    function interpret_word() {
+	read_word();
+	if (m_cur_word == "") {         // If no word, then we're at the end of the string
+	    return 0;
+	}
+
+	var entry = m_dictionary[m_cur_word];
+	                                // Look up entry in dictionary
+
+	if (entry) {                    // If we have an entry, execute its code
+	    entry.code();
+	}
+	else {                          // Otherwise, see if it's a number
+	    var number = Number(m_cur_word);
+	    if (isNaN(number)) {
+		abort(m_cur_word + " is not a number");
+		return;
+	    }
+	    m_stack.push(number);       // Push number onto forth stack
+	}
+	return 1;                       // Indicate that we interpreted something
+    }
+
+    //---------------------------------------------------------------------------
+    // Defines builtin words for jsforth interpreter
+    //---------------------------------------------------------------------------
+    function define_builtins() {
+	// Define ".s"
+	m_dictionary[".s"] = {
+	    code: function() {
+		var state = {
+		    dictionary: m_dictionary,
+		    stack: m_stack,
+		    return_stack: m_return_stack,
+		    variables: m_variables,
+		    constants: m_constants
+		};
+		console.log(state);
+	    },
+	    parameters: []
+	};
+
+	// Define "HOWDY"
+	m_dictionary["HOWDY"] = {
+	    code: function() {
+		console.log("ENTRY: HOWDY");
+	    },
+	    paramters: []
+	}
+    }
+
+    define_builtins();
+
+
+    var MAX_ITERATIONS = 5000;          // Max words in string
+
     // jsforth intepreter
     function result(str) {
 	m_input = str;                  // Reset m_input
 	m_input_index = 0;              // Start at the beginning
 
-	read_word();
-	console.log(m_cur_word);
+	var num_iters = 0;
 
-	read_word();
-	console.log(m_cur_word);
-
-	read_word();
-	console.log(m_cur_word);
-	read_word();
-	console.log(m_cur_word);
+	while (interpret_word()) {      // Interpret while there are words in string
+	    num_iters++;
+	    if (num_iters > MAX_ITERATIONS) {
+		break;
+	    }
+	}
 	return;
     }
     return result;
