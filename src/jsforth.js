9b6a8aa836053346f924218ebb70af45d4fb3b69
Rino Jose <rjose@spreadsheetkit.com>
2015-06-11 20:28:00 -0700

    Can read word from string

--- src/jsforth.js
+++ src/jsforth.js
@@ -2,15 +2,74 @@
 // Constructs jsforth interpreter
 //======================================
 $f = (function makeInterpreter() {
-    var m_dictionary = {};
-    var m_stack = [];
-    var m_return_stack = [];
-    var m_variables = {};
+    var m_dictionary = {};              // Contains forth words
+    var m_stack = [];                   // Main stack
+    var m_return_stack = [];            // Return stack (for colon definition execution)
+    var m_variables = {};               // Stores variables (and constants)
 
+    var m_input = "";                   // Current input string
+    var m_input_index = 0;              // Index into m_input
 
-    // Interpreter
+    var m_cur_word = "";                // Last word read
+
+    //---------------------------------------------------------------------------
+    // True if space, tab, or newline
+    //---------------------------------------------------------------------------
+    function is_whitespace(ch) {
+	if (ch == " " || ch == "\t" || ch == "\n") {
+	    return true;
+	}
+	else {
+	    return false;
+	}
+    }
+
+    //---------------------------------------------------------------------------
+    // Reads next word from m_input and stores in m_cur_word
+    //---------------------------------------------------------------------------
+    function read_word() {
+	m_cur_word = "";                // Reset m_cur_word
+
+	// Skip whitespace
+	while(m_input_index < m_input.length) {
+	    var ch = m_input[m_input_index];
+	    if (is_whitespace(ch)) {
+		m_input_index++;
+	    }
+	    else {
+		break;
+	    }
+	}
+
+	// Read chars up to next whitespace
+	while(m_input_index < m_input.length) {
+	    var ch = m_input[m_input_index];
+	    if (is_whitespace(ch)) {
+		m_input_index++;
+		break;
+	    }
+	    else {
+		m_cur_word += ch;
+		m_input_index++;
+	    }
+	}
+    }
+
+    // jsforth intepreter
     function result(str) {
-	console.log("TODO: Interpret forth", str);
+	m_input = str;                  // Reset m_input
+	m_input_index = 0;              // Start at the beginning
+
+	read_word();
+	console.log(m_cur_word);
+
+	read_word();
+	console.log(m_cur_word);
+
+	read_word();
+	console.log(m_cur_word);
+	read_word();
+	console.log(m_cur_word);
 	return;
     }
     return result;
